f3463a9712556fbd79e83e7404f59c988442cfd5
olivergirard <girard1@purdue.edu>
2022-07-22 17:37:07 -0400

    added assets

--- TackyMobs/ModEntry.cs
+++ TackyMobs/ModEntry.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
@@ -9,40 +11,24 @@ namespace TackyMobs
 {
     public class ModEntry : Mod
     {
+        /* Entry function. */
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Content.AssetRequested += this.OnAssetRequested;
-            helper.Events.GameLoop.SaveLoaded += this.SlimeColorChanger;
+            helper.Events.Content.AssetRequested += this.LoadAsset;
         }
 
-        private void OnAssetRequested(object sender, AssetRequestedEventArgs e)
+        private void LoadAsset(object sender, AssetRequestedEventArgs e)
         {
-            if (e.NameWithoutLocale.IsEquivalentTo("Characters/Monsters/Ghost"))
+            if (e.Name.IsEquivalentTo("Characters/Monsters/Ghost"))
             {
-                e.Edit(asset =>
-                {
-                    var editor = asset.AsImage();
-                    e.LoadFromModFile<IAssetDataForImage>("assets/Ghost.png", AssetLoadPriority.Medium);
-                });
-            }
-            else if (e.NameWithoutLocale.IsEquivalentTo("Characters/Monsters/Carbon Ghost"))
+                e.LoadFromModFile<Texture2D>("assets/Ghost.png", AssetLoadPriority.Medium);
+            } else if (e.Name.IsEquivalentTo("Characters/Monsters/Carbon Ghost"))
             {
-                e.Edit(asset =>
-                {
-                    var editor = asset.AsImage();
-                    e.LoadFromModFile<IAssetDataForImage>("assets/Carbon Ghost.png", AssetLoadPriority.Medium);
-                });
-            }
-        }
-
-        private void SlimeColorChanger(object sender, SaveLoadedEventArgs e)
-        {
-
-            //this may not work, its from autoquarrymod
-            StardewValley.Object monster;
-
-            //do the same for loop struct thing?
-
+                e.LoadFromModFile<Texture2D>("assets/Carbon Ghost.png", AssetLoadPriority.Medium);
+            } else if (e.Name.IsEquivalentTo("Characters/Monsters/Grub"))
+            {
+                e.LoadFromModFile<Texture2D>("assets/Grub.png", AssetLoadPriority.Medium);
+            } 
         }
     }
 }
