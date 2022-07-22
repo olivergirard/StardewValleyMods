2003732dc07761fef0588526e7bb8eb5d059fc25
olivergirard <girard1@purdue.edu>
2022-07-22 17:36:48 -0400

    added tree removal

--- AutoQuarry/ModEntry.cs
+++ AutoQuarry/ModEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
@@ -21,6 +22,7 @@ namespace AutoQuarry
         {
             GameLocation mountain = Game1.getLocationFromName("Mountain");
             StardewValley.Object obby;
+            StardewValley.TerrainFeatures.LargeTerrainFeature terrainFeature;
 
             /* Quarry coordinates determined manually. Subject to change.*/
 
@@ -32,7 +34,20 @@ namespace AutoQuarry
                    
                     if (obby != null)
                     {
-                        AddToInventory(obby);
+                        AddResourceToInventory(obby);
+
+                    } else if (mountain.isTerrainFeatureAt(x, y))
+                    {
+                        terrainFeature = mountain.getLargeTerrainFeatureAt(x, y);
+                        StardewValley.TerrainFeatures.TerrainFeature temp = (StardewValley.TerrainFeatures.TerrainFeature)terrainFeature;
+                        StardewValley.TerrainFeatures.Tree tree = (StardewValley.TerrainFeatures.Tree) temp;
+
+
+                        //if tree.growthStage doesnt exist w3hat happen?
+                        if ((tree.growthStage >= 5) && (tree.tapped == false))
+                        {
+                            AddTreeToInventory(tree);
+                        }
                     }
                 }
             }
@@ -40,7 +55,7 @@ namespace AutoQuarry
 
         /* Determines what resource and how much of it to add to the player's inventory. */
 
-        private void AddToInventory(StardewValley.Object obby)
+        private void AddResourceToInventory(StardewValley.Object obby)
         {
             Game1.getLocationFromName("Mountain").removeObject(obby.TileLocation, false);
 
@@ -56,8 +71,7 @@ namespace AutoQuarry
              * case 46: mystic node
              * case 765: iridium node
              * cases 32-847: different types of stones
-             * cases 752-758: different types of boulders 
-             */
+             * cases 752-758: different types of boulders */
 
             switch (obby.ParentSheetIndex)
             {
@@ -193,6 +207,100 @@ namespace AutoQuarry
             }
 
             return gemID;
+        }
+
+        private void AddTreeToInventory(StardewValley.TerrainFeatures.Tree tree)
+        {
+
+            Game1.getLocationFromName("Mountain").OnTerrainFeatureRemoved(tree);
+
+            var rand = new Random();
+
+            /* Trees that naturally grow in the quarry
+             * 1 - oak tree
+             * 2 - maple tree
+             * 3 - pine tree
+             * 4 - winter oak tree
+             * 5 - winter maple tree
+             * 8 - mahogany tree */
+
+            Game1.player.addItemByMenuIfNecessary(new StardewValley.Object(92, 5, false, -1, 0));
+
+            int seedCount;
+            int hardwoodCount = 0;
+
+            if ((tree.treeType >= 1) && (tree.treeType <= 5))
+            {
+                Game1.player.addItemByMenuIfNecessary(new StardewValley.Object(388, WoodCount(), false, -1, 0));
+
+                seedCount = rand.Next(0, 3);
+
+                switch (tree.treeType) {
+                    case 1:
+                    case 4:
+                        Game1.player.addItemByMenuIfNecessary(new StardewValley.Object(309, seedCount, false, -1, 0));
+                        break;
+                    case 2:
+                    case 5:
+                        Game1.player.addItemByMenuIfNecessary(new StardewValley.Object(310, seedCount, false, -1, 0));
+                        break;
+                    case 3:
+                        Game1.player.addItemByMenuIfNecessary(new StardewValley.Object(311, seedCount, false, -1, 0));
+                        break;
+                }
+
+                /* If player's foraging skill (2) has the lumberjack profession (14). */
+
+                while ((Game1.player.professions.Contains(14)) && rand.NextDouble() < 0.5)
+                {
+                    hardwoodCount++;
+                }
+                Game1.player.addItemByMenuIfNecessary(new StardewValley.Object(709, hardwoodCount, false, -1, 0));
+
+            } else if (tree.treeType == 8)
+            {
+                seedCount = rand.Next(0, 2);
+                Game1.player.addItemByMenuIfNecessary(new StardewValley.Object(292, seedCount, false, -1, 0));
+
+                hardwoodCount = 10;
+
+                if (Game1.player.professions.Contains(12))
+                {
+                    hardwoodCount *= (int) 1.25;
+                }
+                Game1.player.addItemByMenuIfNecessary(new StardewValley.Object(709, hardwoodCount, false, -1, 0));
+            }
+        }
+
+        /* Used only for oak, maple, or pine trees. Mahogany trees do not drop this wood. */
+        private int WoodCount()
+        {
+            int woodCount = 12;
+            var rand = new Random();
+
+            if (rand.NextDouble() < Game1.player.DailyLuck)
+            {
+                woodCount++;
+            }
+            if (rand.NextDouble() < (double)Game1.player.ForagingLevel / 12.5)
+            {
+                woodCount++;
+            }
+            if (rand.NextDouble() < (double)Game1.player.ForagingLevel / 12.5)
+            {
+                woodCount++;
+            }
+            if (rand.NextDouble() < (double)Game1.player.LuckLevel / 25.0)
+            {
+                woodCount++;
+            }
+
+            if (Game1.player.professions.Contains(14))
+            {
+                woodCount *= (int) 1.25;
+            }
+
+            return woodCount;
         }
     }
 }
