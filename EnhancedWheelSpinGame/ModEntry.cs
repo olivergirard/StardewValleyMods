2fbafc8189390f450bafc1dddd8f466ea8d8dafa
olivergirard <girard1@purdue.edu>
2022-08-15 22:33:50 -0400

    added new mod

---
+++ EnhancedWheelSpinGame/ModEntry.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using HarmonyLib;
+
+namespace EnhancedWhenSpinGame
+{
+    public class ModEntry : Mod
+    {
+
+        /* Entry point. */
+
+        public override void Entry(IModHelper helper)
+        {
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+
+            harmony.Patch(
+               original: AccessTools.Method(typeof(StardewValley.Menus.WheelSpinGame), nameof(StardewValley.Menus.WheelSpinGame.update)),
+               prefix: new HarmonyMethod(typeof(EnhancedWheelSpinGame.WheelSpinGame), nameof(EnhancedWheelSpinGame.WheelSpinGame.NewWheel))
+            );
+
+            harmony.Patch(
+               original: AccessTools.Method(typeof(StardewValley.Event), nameof(StardewValley.Event.checkAction)),
+               prefix: new HarmonyMethod(typeof(EnhancedWheelSpinGame.WheelSpinGame), nameof(EnhancedWheelSpinGame.WheelSpinGame.NewAction))
+            );
+
+            EnhancedWheelSpinGame.WheelSpinGame.Initialize(this.Monitor);
+
+            helper.Events.GameLoop.SaveLoaded += this.Debug;
+            helper.Events.Content.AssetRequested += this.LoadWheelAssets;
+        }
+
+        /* Changes the .png files that contain the wheel. */
+
+        private void LoadWheelAssets(object sender, AssetRequestedEventArgs e)
+        {
+            if (e.Name.StartsWith("LooseSprites/", false, true))
+            {
+                if (e.Name.IsEquivalentTo("LooseSprites/Cursors"))
+                {
+                    e.LoadFromModFile<Texture2D>("assets/Cursors.png", AssetLoadPriority.Medium);
+                }
+                else if (e.Name.IsEquivalentTo("LooseSprites/Festivals"))
+                {
+                    e.LoadFromModFile<Texture2D>("assets/Festivals.png", AssetLoadPriority.Medium);
+                }
+            }
+        }
+
+        /* TODO remove */
+        private void Debug(object sender, SaveLoadedEventArgs e)
+        {
+            StardewValley.WorldDate newDate = new StardewValley.WorldDate();
+            newDate.DayOfMonth = 16;
+        }
+
+
+    }
+}
---
+++ EnhancedWheelSpinGame/WheelSpinGame.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using HarmonyLib;
+using System.Threading;
+using System;
+using StardewValley;
+using xTile.Dimensions;
+
+namespace EnhancedWheelSpinGame
+{
+    internal class WheelSpinGame
+    {
+        private static IMonitor Monitor;
+        private static string colorPicked;
+
+        // call this method from your Entry class
+        public static void Initialize(IMonitor monitor)
+        {
+            Monitor = monitor;
+        }
+
+        //TODO change colors (array) to hold all colors
+        //that picked color sets a class variable colorPicked to a color string blue, pink, green, or orange
+        //run newwheel method based off of that
+
+        public static bool NewWheel()
+        {
+            try
+            {
+                
+
+
+
+
+
+
+
+
+                return false; // don't run original logic
+            }
+            catch (Exception ex)
+            {
+                Monitor.Log($"Failed in {nameof(NewWheel)}:\n{ex}", LogLevel.Error);
+                return true; // run original logic
+            }
+        }
+
+        public static bool NewAction(Location tileLocation, xTile.Dimensions.Rectangle viewport, Farmer farmer)
+        {
+            try
+            {
+
+
+
+
+
+
+
+
+
+                return false; // don't run original logic
+            }
+            catch (Exception ex)
+            {
+                Monitor.Log($"Failed in {nameof(NewWheel)}:\n{ex}", LogLevel.Error);
+                return true; // run original logic
+            }
+        }
+    }
+}
