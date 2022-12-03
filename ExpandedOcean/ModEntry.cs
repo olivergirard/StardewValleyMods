7d07b251a2ee59e5f2f76fda7a2d10ca766c30ea
olivergirard <girard1@purdue.edu>
2022-12-02 20:38:54 -0500

    added sunfish

--- ExpandedOcean/ModEntry.cs
+++ ExpandedOcean/ModEntry.cs
@@ -13,7 +13,6 @@ namespace ExpandedOcean
         public static bool objectDataAdded = false;
         public static bool locationDataAdded = false;
 
-
         /* Entry point. */
 
         public override void Entry(IModHelper helper)
@@ -50,7 +49,7 @@ namespace ExpandedOcean
 
                         //TODO Edit values to MinimumDepth = 1 SpawnMultiplier = 1 DepthMultiplier = 1 during debugging. Change seasons/time if necessary.
 
-                        //editor.Data.Add("936", "Ocean Sunfish/70/smooth/70/121/600 1900/summer fall/both/ /3/.2/.3/0");
+                        editor.Data.Add(936, "Ocean Sunfish/70/smooth/70/121/600 1900/summer fall/both/ /3/.2/.3/0");
                         editor.Data.Add(937, "Chimaera/80/sinker/24/80/1900 2600/spring summer/both/ /1/1/1/5");
                     });
 
@@ -70,7 +69,7 @@ namespace ExpandedOcean
                         /* Format: Name/Price/Edibility/TypeAndCategory/DisplayName/Description/TimesAvailable^SeasonsAvailable
                          * More info here: https://stardewcommunitywiki.com/Modding:Object_data */
 
-                        //editor.Data.Add("936", "Ocean Sunfish/800/15/Fish -4/Ocean Sunfish/A very heavy and very bony fish./Day Night^Summer Fall");
+                        editor.Data.Add(936, "Ocean Sunfish/800/15/Fish -4/Ocean Sunfish/A very heavy and very bony fish./Day Night^Summer Fall");
                         editor.Data.Add(937, "Chimaera/500/2/Fish -4/Chimaera/A bottom-dwelling, deep-sea fish./Night^Spring Summer");
                     });
 
@@ -95,10 +94,9 @@ namespace ExpandedOcean
                         /* These are the only strings that need to be edited. */
 
                         string springInfo = "129 -1 131 -1 147 -1 148 -1 152 -1 708 -1 267 -1 937 -1/";
-                        string summerInfo = "128 -1 130 -1 146 -1 149 -1 150 -1 152 -1 155 -1 708 -1 701 -1 267 -1 937 -1/";
-                        string fallInfo = "129 - 1 131 - 1 148 - 1 150 - 1 152 - 1 154 - 1 155 - 1 705 - 1 701 - 1/";
+                        string summerInfo = "128 -1 130 -1 146 -1 149 -1 150 -1 152 -1 155 -1 708 -1 701 -1 267 -1 937 -1 936 -1/";
+                        string fallInfo = "129 - 1 131 - 1 148 - 1 150 - 1 152 - 1 154 - 1 155 - 1 705 - 1 701 - 1 936 -1/";
                         string winterInfo = "708 - 1 130 - 1 131 - 1 146 - 1 147 - 1 150 - 1 151 - 1 152 - 1 154 - 1 705 - 1/";
-
 
                         string updatedBeach = beachInfo + springInfo + summerInfo + fallInfo + winterInfo + artifactData;
 
@@ -106,7 +104,6 @@ namespace ExpandedOcean
                     });
 
                     fishDataAdded = true;
-
                 }
             }
         }
@@ -122,6 +119,11 @@ namespace ExpandedOcean
                 fish = Texture2D.FromFile(Game1.graphics.GraphicsDevice, "assets/Chimaera.png");
                 Game1.spriteBatch.Draw(fish, Game1.GlobalToLocal(Game1.viewport, Game1.player.Position + new Vector2(0f, -56f)), Color.White);
                 return new Object(937, 1);
+            } else if (result.Name.Equals("Ocean Sunfish"))
+            {
+                fish = Texture2D.FromFile(Game1.graphics.GraphicsDevice, "assets/Ocean Sunfish.png");
+                Game1.spriteBatch.Draw(fish, Game1.GlobalToLocal(Game1.viewport, Game1.player.Position + new Vector2(0f, -56f)), Color.White);
+                return new Object(936, 1);
             }
 
             return result;
